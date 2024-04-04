d3cf27babf16b819f64e267553bb902858e88bf6
Jakub G <jakub.glowny@gmail.com>
2024-04-04 21:20:48 +0200

    add repo to github

---
+++ src/Table.tsx
@@ -0,0 +1,64 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import TableSortLabel from '@mui/material/TableSortLabel';
+type Props = {
+    data: [];
+    handleSort(field: string, order: string): void;
+    sortName(): void;
+    sortCount(): void;
+};
+
+type dataProps = {
+    name: string;
+    count: number;
+};
+
+export default function BasicTable({ data, handleSort, sortName, sortCount }: Props) {
+    return (
+        <>
+            <TableContainer component={Paper} sx={{ marginTop: '20px' }}>
+                <Table sx={{ minWidth: 450 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">
+                            <TableSortLabel
+                                active={true}
+                                direction={sortName}
+                                onClick={() => handleSort('name',sortName)}
+                                >Name
+                            </TableSortLabel>
+
+                            </TableCell>
+                            <TableCell align="right">
+                            <TableSortLabel
+                                active={true}
+                                direction={sortCount}
+                                onClick={() => handleSort('count',sortCount)}
+                                >Questions
+                            </TableSortLabel>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map(({ name, count }: dataProps) => (
+                            <TableRow key={`row-${name}`} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell align="left">
+                                    <Link href={`https://stackoverflow.com/questions/tagged/${name}`}target="_blank" title={name}>
+                                        {name}
+                                    </Link>
+                                </TableCell>
+                                <TableCell align="right">{count}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    );
+}
---
+++ src/Tag.tsx
@@ -0,0 +1,20 @@
+
+type Props = {
+    name: string,
+    description?: string,
+    questions?: number;
+}
+
+export const Tag = (
+    { 
+    name = 'javascript', 
+    questions = 2528741, 
+    }: Props) => {
+    return (
+        <div className='tag-box'>
+            <a>{name}</a>
+            <p>{questions} questions</p>
+          </div>
+    )
+}
+
